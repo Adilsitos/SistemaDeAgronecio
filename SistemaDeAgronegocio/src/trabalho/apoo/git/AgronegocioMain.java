691f78da5b7b556d0c2b500f3ba4f865d233b00f
adils <adils@192.168.22.4>
2018-09-29 13:47:45 -0300

    add line

--- SistemaDeAgronegocio/src/trabalho/apoo/git/AgronegocioMain.java
+++ SistemaDeAgronegocio/src/trabalho/apoo/git/AgronegocioMain.java
@@ -5,7 +5,7 @@ public class AgronegocioMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testando o Git");
-
+		System.out.println("teste commit");
 	}
 
 }
