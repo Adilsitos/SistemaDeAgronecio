3754065f4547d9b3cd5cffec86b9bd399c19ef45
Alexandre de Souza Sampaio Filho <32207196+xandea@users.noreply.github.com>
2018-11-07 17:18:34 -0200

    Add files via upload

---
+++ ProjetoApoo/src/InData.java
@@ -0,0 +1,27 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+public class InData{
+
+	public String entDados(String rotulo){
+		InputStreamReader teclado = new InputStreamReader(System.in);
+		BufferedReader buff = new BufferedReader(teclado);
+		String ret = "";
+
+		System.out.println(rotulo);
+
+		try{
+			ret = buff.readLine();
+		}
+
+		catch(IOException ioe){
+			System.out.println("\n Erro de sistema\n");
+		}
+
+		return ret;
+	}
+
+}
+
