237cc5f0cb4edfc1213af8a03e46c47051149036
angelospi <angelo.spinardi@hotmail.com>
2018-11-13 19:03:00 -0200

    Add files via upload

---
+++ Animal/src/Agro/BDConect.java
@@ -0,0 +1,258 @@
+package Agro;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.lang.Class; // pode-se usar diretamente sem fazer a importa��o
+
+
+public class BDConect{
+	static Connection connection = null;
+	static ResultSet result      = null;
+	static Statement statement   = null;
+        
+ 
+	//Dados do SGBD
+	static String  usuario = "postgres";
+	//static String  senha  = "postgres"; //- Postgres 9.???
+	static String  senha  = "asn1998";
+	//static String url = "jdbc:postgresql://localhost:5432/BDCom"; // nome da fonte de dados - Postgres 9.??
+	static String url = "jdbc:postgresql://localhost:5432/agro"; // nome da fonte de dados - Postgres 10.??
+	
+
+	//m�todo para estabelecer uma conex�o com SGBD
+
+	public static void getConect(){
+		try{
+			Class classe = Class.forName("org.postgresql.Driver"); //encontrar para outros sgbd onde esta o .driver dele
+
+			//System.out.println("\nClasse - toString   : "+classe.toString());
+			//System.out.println("\nClasse - getName : "+classe.getName());
+
+			connection = DriverManager.getConnection(url,usuario,senha);
+
+			statement = connection.createStatement();
+		}
+
+		catch(ClassNotFoundException erro){
+			System.out.println("\n\nNao encontrou o Driver do BD --> "+erro+"!");
+                        		erro.printStackTrace();
+
+		}
+		catch(SQLException erro){
+			System.out.println("\n\nProblemas de conex�o do SQL--> "+erro+"!");
+			System.exit(0);
+		}
+	}
+
+	public static void main(String args[]){
+		//inSQL("insert into clientes(cpf, nome) values (4, 'brhue')");
+		//updSQL("delete from clientes where pk_cli = 4");
+		//updSQL("update clientes set debito = 0 where pk_cli = 700");
+		//updSQL("update clientes set salario = 0 where pk_cli = 700");
+		//System.out.print(setResult("select fis_cpf from fisica where pes_login = 'aaa' and pes_senha = 123 "));
+                // int y = BuscaCnpjJur("select cnpj from pessoa where pes_login = 'aa' and pes_senha =4444 ");
+                // System.out.println(y);
+                
+	}
+	
+
+
+
+	//metodo para a execucao de uma inser��o
+	public static Boolean inSQL(String sql){
+		boolean boolIns = true;
+		try{
+			getConect();
+			boolIns = statement.execute(sql);//classe statement: execute (retorna booleano), result(recebe)(int) = executeQuery(retorna conj. de registros), recebe(int) = executeUpdate(retorna a quantidade de registros afetados)
+                        System.out.println(boolIns);
+                }
+		catch(SQLException erro){
+			System.out.println("\n\nErro de SQL BDConecta "+erro+"! -- de SQL em BDConecta");
+		}
+		close();
+                return boolIns;
+	}
+
+
+	//metodo para a execucao de uma atualiza��o
+	public static int updSQL(String sql){
+		int intUpd = 0;
+		try{
+			getConect();
+			intUpd = statement.executeUpdate(sql);
+              
+		}
+		catch(SQLException erro){
+			System.out.println("Erro ao Atualizar! "+erro);
+		}
+		close();
+                return intUpd;
+	}
+
+
+	//metodo para a execucao de uma consulta
+	public static int BuscaCpfFis(String sql){
+		ResultSet result = null;
+                   int x = 0;
+		try{
+			getConect();
+			result = statement.executeQuery(sql);
+			close();
+			
+		}	
+		catch(SQLException erro){
+			System.out.println("\n\nErro de SQL BDConecta "+erro+"! -- de SQL em BDConecta");
+			System.exit(0);
+		}
+       
+                
+		try{
+			while(result.next()){
+                             x = +result.getInt("cpf");
+                            //System.out.println("\n-CPF:"+ result.getInt("cpf") + "\n-Nome:"+result.getString("nome")+ "\n-Idade:"+result.getInt("idade")+ "\n-Sexo:"+result.getString("sexo"));       
+			}
+		}
+		catch(SQLException errSql){
+			System.out.println("Error de SQL "+errSql+"! -- Erro na Consulta");
+			System.exit(0);	
+		}
+      
+               return x;
+    	}
+        
+        public static int BuscaCnpjJur(String sql){
+		ResultSet result = null;
+                   int x = 0;
+		try{
+			getConect();
+			result = statement.executeQuery(sql);
+			close();
+			
+		}	
+		catch(SQLException erro){
+			System.out.println("\n\nErro de SQL BDConecta "+erro+"! -- de SQL em BDConecta");
+			System.exit(0);
+		}
+       
+                
+		try{
+			while(result.next()){
+                             x = +result.getInt("cnpj");
+                           }
+		}
+		catch(SQLException errSql){
+			System.out.println("Error de SQL "+errSql+"! -- Erro na Consulta");
+			System.exit(0);	
+		}
+      
+               return x;
+    	}
+        
+        public static void setAniResult(String sql){
+		ResultSet result = null;
+		try{
+			getConect();
+			result = statement.executeQuery(sql);
+			close();
+			
+		}	
+		catch(SQLException erro){
+			System.out.println("\n\nErro de SQL BDConecta "+erro+"! -- de SQL em BDConecta");
+			System.exit(0);
+		}
+		try{
+			while(result.next()){
+				System.out.println("\n-Codigo:"+ result.getInt("cod_an") + "\n-Nome:"+result.getString("nome")+ "\n-Idade:"+result.getInt("idade")+ "\n-Raca:"+result.getString("raça")+ "\n-Peso:"+result.getInt("peso")+ "\n-Cpf_FK:"+result.getInt("cpf_fk"));
+                  
+			}
+		}
+		catch(SQLException errSql){
+			System.out.println("Error de SQL "+errSql+"! -- Erro na Consulta");
+			System.exit(0);	
+		}
+    	}
+
+/*public static Clientes setBuscaCPF(String sql){
+		ResultSet result = null;
+		try{
+			getConect();
+			result = statement.executeQuery(sql);
+			close();
+			
+		}	
+		catch(SQLException erro){
+			System.out.println("\n\nErro de SQL BDConecta "+erro+"! -- de SQL em BDConecta");
+			System.exit(0);
+		}
+                Clientes cli = new Clientes();
+		try{
+			while(result.next()){
+				//System.out.println("\n-CPF:"+ result.getInt("cpf") + "\n-Nome:"+result.getString("nome")+ "\n-Idade:"+result.getInt("idade")+ "\n-Sexo:"+result.getString("sexo"));       
+                                
+                                
+                                cli.setCpf(result.getInt("cpf"));
+                                cli.setNome(result.getString("nome"));
+                                cli.setIdade(result.getInt("idade"));
+                                cli.setSexo(result.getString("sexo"));
+                                
+                        }
+		}
+		catch(SQLException errSql){
+			System.out.println("Error de SQL "+errSql+"! -- Erro na Consulta");
+			System.exit(0);	
+		}
+                return cli;
+                                
+    	}
+
+public static Animais setBuscaCPFAni(String sql){
+		ResultSet result = null;
+		try{
+			getConect();
+			result = statement.executeQuery(sql);
+			close();
+			
+		}	
+		catch(SQLException erro){
+			System.out.println("\n\nErro de SQL BDConecta "+erro+"! -- de SQL em BDConecta");
+			System.exit(0);
+		}
+                Animais anii = new Animais();
+		try{
+			while(result.next()){
+				//System.out.println("\n-CPF:"+ result.getInt("cpf") + "\n-Nome:"+result.getString("nome")+ "\n-Idade:"+result.getInt("idade")+ "\n-Sexo:"+result.getString("sexo"));       
+                                
+                                
+                                anii.setCodan(result.getInt("cod_an"));
+                                anii.setNome(result.getString("nome"));
+                                anii.setIdade(result.getInt("idade"));
+                                anii.setRaca(result.getString("raça"));
+                                anii.setPeso(result.getInt("peso"));
+                                
+                        }
+		}
+		catch(SQLException errSql){
+			System.out.println("Error de SQL "+errSql+"! -- Erro na Consulta");
+			System.exit(0);	
+		}
+                return anii;
+                                
+    	}*/
+	
+	//metodo para encerrar a conexao
+	public static void close(){
+		
+		try{
+			if(!connection.isClosed()) connection.close();
+		}
+		catch(SQLException erro){
+			System.out.println("\n\nErro ao desconectar a: "+url+"! -- Erro de Desconexao!");
+						
+		}
+	}
+
+}
+
---
+++ Animal/src/Agro/Class_Animal.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Agro;
+
+/**
+ *
+ * @author Angelo
+ */
+public class Class_Animal {
+    private int numRegistro;
+    private int peso;
+    private int idade;
+    private String tipo;
+    private String raca;
+
+    /**
+     * @return the numRegistro
+     */
+    public int getNumRegistro() {
+        return numRegistro;
+    }
+
+    /**
+     * @param numRegistro the numRegistro to set
+     */
+    public void setNumRegistro(int numRegistro) {
+        this.numRegistro = numRegistro;
+    }
+
+    /**
+     * @return the peso
+     */
+    public int getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the raca
+     */
+    public String getRaca() {
+        return raca;
+    }
+
+    /**
+     * @param raca the raca to set
+     */
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+    
+    
+}
